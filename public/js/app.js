12c17eff72667838ef1364d7a3d914baf2bf7157
Yuting Zhang <tinaytz@users.noreply.github.com>
2016-10-30 09:55:55 -0700

    add feedback after adding question

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ var main = function () {
 var $dataArea=$('#dataArea');
 $dataArea.show();
 $("#QAArea").hide();
-
+var answerId;
 var url = "http://api.flickr.com/services/feeds/photos_public.gne?" +
                   "tags=dogs&format=json&jsoncallback=?";
                  // 'http://localhost:3000/question'
@@ -22,7 +22,9 @@ $("#getQestion").click(function(e){
   	var res=JSON.stringify(Response);
   	console.log("res"+res);
  	var html=Response.question;
+ 	answerId=Response.answerId;
  	$dataArea.html(html);
+
  	console.log(Response);//should be stringify--> JSON.stringify(Response)
 	}); 
 
@@ -39,17 +41,42 @@ $("#createQuestion").click(function(){
 
 $("#submitQuestion").click(function(e){
 	e.preventDefault();
+	console.log( $("#question").val());
     $.post("/question",
-    JSON.stringify({
+    {
         'question': $("#question").val(),
         'answer': $("#answer").val()
-    }));
-    $("#createQFb html").text="Sucess"
-    $("#question").val()='';
-    $("#answer").val()='';
+    });
+    $("#createQFb").html("Sucess");
+    document.getElementById('question').value = "";
+    document.getElementById('answer').value = "";
     $("#QAArea").hide();
 });
 
+
+$("#submitAnswer").click(function(e){
+	e.preventDefault();
+	console.log( $("#question").val());
+    $.post("/answer",
+    {
+        'answerId': answerId,
+        'answer': $("#answerQA").val()
+    });
+    // $("#answerQA").text="Sucess"
+    document.getElementById('answerQA').value = "";
+    $.getJSON('/question', function (Response) {
+ 	// we'll simply print the response to the console // for the time being console.log(flickrResponse);
+  	var res=JSON.stringify(Response);
+  	console.log("res"+res);
+ 	var html=Response.question;
+ 	answerId=Response.answerId;
+ 	$dataArea.html(html);
+
+ 	console.log(Response);//should be stringify--> JSON.stringify(Response)
+	}); 
+
+    
+});
 
 };
    
